a639ff071ef23a5099e11a05eaefbf95f4de3ff8
ndelucca <nazadelucca@gmail.com>
2024-09-14 15:18:12 -0300

    jellybean colors

--- lua/ndelucca/packer.lua
+++ lua/ndelucca/packer.lua
@@ -10,4 +10,5 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use 'nanotech/jellybeans.vim' 
 end)
