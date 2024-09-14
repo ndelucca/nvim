355fa09f879c608642340c136af47de0bc673601
ndelucca <nazadelucca@gmail.com>
2024-09-14 15:53:39 -0300

    treesitter

--- lua/ndelucca/packer.lua
+++ lua/ndelucca/packer.lua
@@ -6,9 +6,10 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'navarasu/onedark.nvim'
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use 'navarasu/onedark.nvim'
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
 end)
