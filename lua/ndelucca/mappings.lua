66bf494227c2bc154c34d395d979172f5d6c958a
ndelucca <nazadelucca@gmail.com>
2024-09-20 00:50:25 -0300

    format keymappings

--- lua/ndelucca/mappings.lua
+++ lua/ndelucca/mappings.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "-", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-u>","<C-u>zz",{noremap = true})
 vim.keymap.set("n", "<C-d>","<C-d>zz",{noremap = true})
+vim.keymap.set({'n','v'}, '<leader>I', function() vim.lsp.buf.format({async = true})end, {noremap = true})
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
