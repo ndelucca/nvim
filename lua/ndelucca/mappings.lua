30163aa9b67e993dad51c892714d57c84781363f
ndelucca <nazadelucca@gmail.com>
2024-09-21 19:12:56 -0300

    trying out new keymappings

--- lua/ndelucca/mappings.lua
+++ lua/ndelucca/mappings.lua
@@ -2,11 +2,15 @@ vim.keymap.set("n", "-", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set('n', '<leader>ww', ':bd<CR>', { silent = true })
+vim.keymap.set('n', '<leader>fww', ':bd!<CR>', { silent = true })
+
+-- Formatters
 vim.keymap.set({ 'n', 'v' }, '<leader>I', function() vim.lsp.buf.format({ async = true }) end, { noremap = true })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>ph', builtin.live_grep, {})
+vim.keymap.set('n', '<C-P>', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.grep_string, {})
+vim.keymap.set('n', '<C-F>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
