9639e249337e605ab689c0a231d5eb034f2791bb
ndelucca <nazadelucca@gmail.com>
2024-09-22 18:33:49 -0300

    lazy setup to separate file and new theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,5 +1,13 @@
 return {
     {
+        'rebelot/kanagawa.nvim',
+        config = function()
+            vim.cmd.colorscheme("kanagawa-dragon")
+            -- vim.cmd.colorscheme("kanagawa-wave")
+            -- vim.cmd.colorscheme("kanagawa-lotus")
+        end
+    },
+    {
         'navarasu/onedark.nvim',
         config = function()
             require('onedark').setup {
