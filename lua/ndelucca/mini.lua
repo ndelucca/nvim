3da97e41db8b3afe818c70f721bdb30d6fc3d603
ndelucca <nazadelucca@gmail.com>
2024-09-23 22:27:47 -0300

    mini.comment

---
+++ lua/ndelucca/mini.lua
@@ -0,0 +1,8 @@
+require('mini.comment').setup({
+    mappings = {
+        comment = '',
+        comment_line = '<leader>cl',
+        comment_visual = '<leader>cl',
+        textobject = '',
+    },
+})
