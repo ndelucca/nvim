6bfffe9cbdd32e3864281fbb08a29af0f3e95480
ndelucca <ndelucca@educativa.com>
2024-10-02 09:42:32 -0300

    new theme trial period

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,8 +1,23 @@
 return {
     {
-        'rebelot/kanagawa.nvim',
+        'olivercederborg/poimandres.nvim',
+        lazy = false,
+        priority = 1000,
         config = function()
-            vim.cmd.colorscheme("kanagawa-dragon")
+            require('poimandres').setup {
+                -- leave this setup function empty for default config
+                -- or refer to the configuration section
+                -- for configuration options
+            }
+        end,
+        init = function()
+            vim.cmd("colorscheme poimandres")
         end
+    -- },
+    -- {
+    --     'rebelot/kanagawa.nvim',
+    --     config = function()
+    --         vim.cmd.colorscheme("kanagawa-dragon")
+    --     end
     },
 }
