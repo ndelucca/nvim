5fbddb96a01265415438759517df0ce4d768566c
ndelucca <nazadelucca@gmail.com>
2024-10-03 00:41:46 -0300

    replace replace macros

--- lua/ndelucca/macros.lua
+++ lua/ndelucca/macros.lua
@@ -1,19 +1,3 @@
-local function interactive_replace()
-    vim.cmd('normal! "xy')
-    local selected_text = vim.fn.getreg('x')
-    local replacement_text = vim.fn.input("Replacement: ")
-    vim.cmd("execute ':%s/" .. selected_text .. "/" .. replacement_text .. "/gc'")
-end
-
-local function interactive_search_replace()
-    local selected_text = vim.fn.input("Search: ")
-    local replacement_text = vim.fn.input("Replacement: ")
-    vim.cmd("execute ':%s/" .. selected_text .. "/" .. replacement_text .. "/gc'")
-end
-
-vim.keymap.set('v', '<leader>fd', function() interactive_replace() end, { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>ff', function() interactive_search_replace() end, { noremap = true, silent = false })
-
 vim.api.nvim_create_user_command(
     'ToggleSplitStruct',
     function()
--- lua/ndelucca/mappings.lua
+++ lua/ndelucca/mappings.lua
@@ -11,3 +11,9 @@ vim.keymap.set({ 'n', 'v' }, '<leader>I', function() vim.lsp.buf.format({ async 
 vim.keymap.set('n', '<C-j>', ':bprev<CR>', { noremap = true })
 vim.keymap.set('n', '<C-k>', ':bnext<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>qw', ':bufdo bd<CR>', { noremap = true })
+
+-- Replace text ocurrences
+vim.keymap.set("n", '<leader>rt', '*:%s///g<left><left>', { noremap = true })
+vim.keymap.set("v", '<leader>rt', '"hy:%s/<C-r>h//g<left><left>', { noremap = true })
+-- Confirm replace text one by one
+vim.keymap.set("n", '<leader>rc', '*:%s///gc<left><left><left>', { noremap = true })
