497ead185f42934d5ba4034a39e1f650787d0cb6
ndelucca <nazadelucca@gmail.com>
2024-10-07 12:44:56 -0300

    quickfix basic handling

--- lua/ndelucca/mappings.lua
+++ lua/ndelucca/mappings.lua
@@ -7,8 +7,14 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 -- vim.keymap.set('n', '<leader>wq', ':bd!<CR>', { silent = true })
 -- vim.keymap.set('n', '<leader>qw', ':bufdo bd<CR>', { noremap = true })
 
-vim.keymap.set('n', '<C-j>', ':bprev<CR>', { noremap = true })
-vim.keymap.set('n', '<C-k>', ':bnext<CR>', { noremap = true })
+vim.keymap.set('n', '<C-h>', ':bprev<CR>', { noremap = true })
+vim.keymap.set('n', '<C-l>', ':bnext<CR>', { noremap = true })
+
+vim.keymap.set('n', '<C-j>', ':cprev<CR>', { noremap = true })
+vim.keymap.set('n', '<C-k>', ':cnext<CR>', { noremap = true })
+
+vim.keymap.set('n', '<leader>co', ':copen<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>cc', ':cclose<CR>', { noremap = true })
 
 -- Replace text ocurrences
 vim.keymap.set("n", '<leader>rt', '*:%s///g<left><left>', { noremap = true })
