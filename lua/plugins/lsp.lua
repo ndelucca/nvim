e1c8ea4848c6805fd0bdf5e281145d46d8d31912
ndelucca <ndelucca@educativa.com>
2025-01-08 15:43:08 -0300

    ansible linter configured

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,9 +18,56 @@ local lsp_keymaps = function(client, bufnr)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 end
 
+local create_autocmd = vim.api.nvim_create_autocmd
+local ansible_autogroup = vim.api.nvim_create_augroup('ansible_autogroup', {})
+
+create_autocmd('BufEnter', {
+    pattern = { '*.yml', '*.yaml' },
+    command = 'setl ft=yaml.ansible',
+    group = ansible_autogroup
+})
+
+create_autocmd('FileType', {
+    pattern = 'yaml.ansible',
+    command = 'setl tabstop=2 shiftwidth=2 softtabstop=2',
+    group = ansible_autogroup
+})
+
 return {
     {
         "neovim/nvim-lspconfig",
+    },
+    {
+        'pearofducks/ansible-vim', -- Install Ansible syntax
+        lazy = false,              -- Force load
+        config = function()
+            -- Disable auto unindent
+            vim.g.ansible_unindent_after_newline = 0
+
+            -- Set dim and more colors
+            vim.g.ansible_name_highlight = 'd'
+            vim.g.ansible_extra_keywords_highlight = 1
+            vim.g.ansible_attribute_highlight = 'a'
+
+            --- Disable highlight for ansiblels if exists
+            local lspconfig_status, lspconfig = pcall(require, 'lspconfig')
+            if lspconfig_status and lspconfig.ansiblels then
+                lspconfig.ansiblels.setup {
+                    on_attach = function(client, buffern)
+                        client.server_capabilities.semanticTokensProvider = nil
+                    end
+                }
+            end
+
+            --- Disable yaml highlight for treesitter if exists
+            local treesitter_status, treesitter = pcall(require, 'nvim-treesitter.configs')
+            if treesitter_status then
+                require 'nvim-treesitter.configs'.setup {
+                    highlight = { disable = { 'yaml' }, },
+                }
+            end
+        end,
+        ft = 'yaml.ansible',
     },
     {
         "williamboman/mason.nvim",
@@ -45,13 +92,6 @@ return {
                     require('lspconfig')[server_name].setup({ on_attach = lsp_keymaps, })
                 end,
 
-                ["ansiblels"] = function()
-                    require('lspconfig').ansiblels.setup({
-                        filetypes = {
-                            "yaml.ansible", "yml", "yaml"
-                        }
-                    })
-                end,
 
                 ["lua_ls"] = function()
                     require("lspconfig").lua_ls.setup { on_attach = lsp_keymaps, settings = {
