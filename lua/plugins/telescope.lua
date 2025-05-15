cadc5cca7d66a2df8ead641cce844ab9c6c7b775
ndelucca <ndelucca@educativa.com>
2025-05-15 19:19:05 +0200

    buffer keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
 
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
             vim.keymap.set("n", "<leader>fg", live_grep_args_shortcuts.grep_word_under_cursor)
