3738e4eaae782dffc6d63820652b2e3cd5f9a9e2
ndelucca <nazadelucca@gmail.com>
2025-10-05 19:05:06 -0300

    remove pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             require('mini.cursorword').setup({})
             require('mini.move').setup({})
-            require('mini.pairs').setup({})
+            -- require('mini.pairs').setup({})
             require('mini.splitjoin').setup({})
             require('mini.files').setup({ options = { use_as_default_command = true, } })
             require('mini.icons').setup({})
