95e818f9d7cfd5e6a49a6f06effe05c8a0b2881a
ndelucca <nazadelucca@gmail.com>
2025-10-28 17:42:05 -0300

    cleanup

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -3,13 +3,11 @@ return {
         'rmagatti/auto-session',
         lazy = true,
         event = "VimEnter",
-
         ---enables autocomplete for opts
         ---@module "auto-session"
         ---@type AutoSession.Config
         opts = {
             suppressed_dirs = { '~/', '~/dev', '~/Downloads', '/' },
-            -- Removed session_lens as it depends on Telescope
         }
     }
 }
