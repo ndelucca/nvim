a33a466ead835668e74c29c23b1b4c13977433cd
ndelucca <nazadelucca@gmail.com>
2025-10-30 23:00:08 -0300

    stylua applied

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,14 @@
 return {
     {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         config = function()
-            require('mason').setup()
-        end
+            require("mason").setup()
+        end,
     },
     {
-        'stevearc/conform.nvim',
+        "stevearc/conform.nvim",
         config = function()
-            require('conform').setup({
+            require("conform").setup({
                 formatters_by_ft = {
                     lua = { "stylua" },
                     python = { "ruff_format" },
@@ -22,8 +22,18 @@ return {
                     yaml = { "prettierd" },
                     markdown = { "prettierd" },
                     htmldjango = { "djlint" },
+                    xml = { "xmlformat" },
+                    cs = { "csharpier" },
                 },
                 formatters = {
+                    stylua = {
+                        prepend_args = {
+                            "--indent-type",
+                            "Spaces",
+                            "--indent-width",
+                            "4",
+                        },
+                    },
                     prettierd = {
                         prepend_args = {
                             "--tab-width=4",
@@ -36,9 +46,12 @@ return {
                     },
                     djlint = {
                         prepend_args = {
-                            "--indent", "4",
-                            "--profile", "django",
-                            "--max-line-length", "120",
+                            "--indent",
+                            "4",
+                            "--profile",
+                            "django",
+                            "--max-line-length",
+                            "120",
                             -- Uncomment if template variables inside JS/CSS blocks get broken:
                             -- "--format-css",
                             -- "--format-js",
@@ -50,47 +63,47 @@ return {
                 -- Set log level for debugging
                 log_level = vim.log.levels.ERROR,
             })
-        end
+        end,
     },
     {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.config('lua_ls', {
+            vim.lsp.config("lua_ls", {
                 on_init = function(client)
                     if client.workspace_folders then
                         local path = client.workspace_folders[1].name
-                        if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                        if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
                             return
                         end
                     end
 
-                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
                         runtime = {
-                            version = 'LuaJIT'
+                            version = "LuaJIT",
                         },
                         workspace = {
                             checkThirdParty = false,
                             library = {
                                 vim.env.VIMRUNTIME,
-                                '${3rd}/luv/library',
-                                '${3rd}/busted/library',
-                                vim.api.nvim_get_runtime_file('', true),
-                            }
-                        }
+                                "${3rd}/luv/library",
+                                "${3rd}/busted/library",
+                                vim.api.nvim_get_runtime_file("", true),
+                            },
+                        },
                     })
                 end,
                 settings = {
-                    Lua = {}
-                }
+                    Lua = {},
+                },
             })
 
-            vim.lsp.config('ansiblels', {
+            vim.lsp.config("ansiblels", {
                 on_attach = function(client, _)
                     client.server_capabilities.semanticTokensProvider = nil
-                end
+                end,
             })
 
-            vim.lsp.config('ty', {
+            vim.lsp.config("ty", {
                 settings = {
                     ty = {
                         experimental = {
@@ -103,61 +116,65 @@ return {
 
             local language_servers = {
                 -- python
-                'ty',
-                'ruff',
-                'djlsp',
+                "ty",
+                "ruff",
+                "djlsp",
                 -- web
-                'ts_ls',
-                'html',
-                'cssls',
+                "ts_ls",
+                "html",
+                "cssls",
                 -- perl
-                'perlnavigator',
+                "perlnavigator",
                 -- lua
-                'lua_ls',
+                "lua_ls",
                 -- ansible
-                'ansiblels',
+                "ansiblels",
+                -- xml
+                "lemminx",
+                -- csharp
+                "omnisharp",
             }
 
             for _, ls_name in ipairs(language_servers) do
                 vim.lsp.enable(ls_name)
             end
-        end
+        end,
     },
     {
-        'pearofducks/ansible-vim', -- Install Ansible syntax
-        lazy = false,              -- Force load
+        "pearofducks/ansible-vim", -- Install Ansible syntax
+        lazy = false, -- Force load
         config = function()
             -- Set .yml and .yaml to yaml.ansible
             local create_autocmd = vim.api.nvim_create_autocmd
-            local ansible_autogroup = vim.api.nvim_create_augroup('ansible_autogroup', {})
+            local ansible_autogroup = vim.api.nvim_create_augroup("ansible_autogroup", {})
 
-            create_autocmd('BufEnter', {
-                pattern = { '*.yml', '*.yaml' },
-                command = 'setl ft=yaml.ansible',
-                group = ansible_autogroup
+            create_autocmd("BufEnter", {
+                pattern = { "*.yml", "*.yaml" },
+                command = "setl ft=yaml.ansible",
+                group = ansible_autogroup,
             })
-            create_autocmd('FileType', {
-                pattern = 'yaml.ansible',
-                command = 'setl tabstop=2 shiftwidth=2 softtabstop=2',
-                group = ansible_autogroup
+            create_autocmd("FileType", {
+                pattern = "yaml.ansible",
+                command = "setl tabstop=2 shiftwidth=2 softtabstop=2",
+                group = ansible_autogroup,
             })
 
             -- Disable auto unindent
             vim.g.ansible_unindent_after_newline = 0
 
             -- Set dim and more colors
-            vim.g.ansible_name_highlight = 'ob'
+            vim.g.ansible_name_highlight = "ob"
             vim.g.ansible_extra_keywords_highlight = 1
-            vim.g.ansible_attribute_highlight = 'b'
+            vim.g.ansible_attribute_highlight = "b"
 
             -- Disable yaml highlight for treesitter if exists
-            local treesitter_exists, treesitter_configs = pcall(require, 'nvim-treesitter.configs')
+            local treesitter_exists, treesitter_configs = pcall(require, "nvim-treesitter.configs")
             if treesitter_exists then
-                treesitter_configs.setup {
-                    highlight = { disable = { 'yaml' }, },
-                }
+                treesitter_configs.setup({
+                    highlight = { disable = { "yaml" } },
+                })
             end
         end,
-        ft = 'yaml.ansible',
+        ft = "yaml.ansible",
     },
 }
